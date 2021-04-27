eb9a333b69306e5b081d99a5a62f5e8541a9b294
luhong <luhong@WWXC02JQ07KRL02.local.com>
2021-04-27 18:01:32 +0800

    add rx

--- src/main/java/com/lifeonwalden/sshpush/process/RxProcessor.java
+++ src/main/java/com/lifeonwalden/sshpush/process/RxProcessor.java
@@ -1,13 +1,43 @@
 package com.lifeonwalden.sshpush.process;
 
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
-import com.lifeonwalden.sshpush.bean.HostInfo;
+import com.jcraft.jsch.SftpException;
 import com.lifeonwalden.sshpush.bean.PushStep;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
 public interface RxProcessor {
-    static void process(PushStep step, Map<String, Session> sessionMap) {
+    static void process(PushStep step, Map<String, Session> sessionMap) throws JSchException, SftpException, IOException {
+        Session session = sessionMap.get(step.getHostId());
+        if (null == session) {
+            throw new RuntimeException("Not found host ".concat(step.getHostId()));
+        }
 
+        if (!session.isConnected()) {
+            session.connect();
+        }
+
+        ChannelExec channel = (ChannelExec) session.openChannel("exec");
+        channel.setCommand(step.getExec());
+        channel.setInputStream(null);
+        channel.setOutputStream(System.out);
+        channel.setErrStream(System.err);
+        InputStream is = channel.getInputStream();
+        channel.connect();
+
+        byte[] buffer = new byte[1024];
+        int len = 0;
+
+        StringBuilder result = new StringBuilder();
+        while ((len = is.read(buffer)) != -1) {
+            result.append(new String(buffer, 0, len));
+        }
+        System.out.println(result);
+
+        channel.disconnect();
     }
 }
---
+++ src/test/java/com/lifeonwalden/sshpush/core/RxTest.java
@@ -0,0 +1,9 @@
+package com.lifeonwalden.sshpush.core;
+
+import com.lifeonwalden.sshpush.Main;
+
+public class RxTest {
+    public static void main(String[] args) {
+        Main.main(new String[]{"/home/luhong/IdeaProjects/sshpush/src/test/resources/rx.json"});
+    }
+}
