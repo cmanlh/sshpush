ca6404f72b0e150289fc716b451ae111863970e6
luhong <cmanlh@163.com>
2021-05-11 15:42:00 +0800

    add windows command support

--- src/main/java/com/lifeonwalden/sshpush/process/LxProcessor.java
+++ src/main/java/com/lifeonwalden/sshpush/process/LxProcessor.java
@@ -10,7 +10,11 @@ public interface LxProcessor {
     static void process(PushStep step, Map<String, Session> sessionMap) throws IOException, InterruptedException {
         Runtime runtime = Runtime.getRuntime();
 
-        Process process = runtime.exec(step.getExec());
+        String command = step.getExec();
+        if (System.getProperty("os.name").toLowerCase().indexOf("windows") >= 0) {
+            command = "cmd.exe /c ".concat(step.getExec());
+        }
+        Process process = runtime.exec(command);
         int result = process.waitFor();
 
         if (0 == result) {
